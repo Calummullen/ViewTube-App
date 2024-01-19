0d10077e6ed13c4eb00dee37559ec2c989efbc7f
Calummullen <calummullen1995@gmail.com>
2024-01-19 19:18:47 +0000

    chore: add speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { GeistSans } from "geist/font/sans";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 const defaultUrl = process.env.VERCEL_URL
@@ -23,6 +24,7 @@ export default function RootLayout({
         <main>
           {children}
           <Analytics />
+          <SpeedInsights />
         </main>
       </body>
     </html>
